6c00d6aa3a44a820f9eb55e502adf30f193693f8
Alexander Peshkov <oleksandr.peshkov@headbox.com>
2019-07-10 15:16:57 +0300

    Task8

--- task8/src/app/articles.service.spec.ts
+++ task8/src/app/articles.service.spec.ts
@@ -1,50 +1,62 @@
-import {async, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {async, TestBed } from '@angular/core/testing';
 
 import { ArticlesService } from './articles.service';
+import {Subscription} from "rxjs";
 
 describe('ArticlesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let subscription1: Subscription;
+  let subscription2: Subscription;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    subscription1 = null;
+    subscription2 = null;
+  });
+
+  afterEach(() => {
+    if (subscription1) {
+      subscription1.unsubscribe();
+    }
+    if (subscription2) {
+      subscription2.unsubscribe();
+    }
+  })
 
   it('should be created', () => {
     const service: ArticlesService = TestBed.get(ArticlesService);
     expect(service).toBeTruthy();
   });
 
-  it('should create new article', fakeAsync(() => {
+  it('should create new article', async(() => {
     const service: ArticlesService = TestBed.get(ArticlesService);
-    let created = false;
-    service.articles$.subscribe(result => {
-      if (!created) {
+    let initialized = false;
+    subscription1 = service.articles$.subscribe(result => {
+      if (!initialized) {
         expect(result.length).toBe(2);
+        initialized = true;
       } else {
         expect(result.length).toBe(3);
       }
     });
-    tick(3000);
-    created = true;
     const article$ = service.createArticle$({
       title: 'Article11',
       text: 'Cool Article'
     });
-    tick(3000);
-    article$.subscribe(result => expect(result.id).toBeTruthy())
-    tick(3000);
+    subscription2 = article$.subscribe(result => expect(result.id).toBeTruthy());
   }));
 
-  it('should remove article', fakeAsync(() => {
+  it('should remove article', async(() => {
     const service: ArticlesService = TestBed.get(ArticlesService);
-    let removed = false;
-    service.articles$.subscribe(result => {
-      if (!removed) {
+    let initialized = false;
+    subscription1 = service.articles$.subscribe(result => {
+      if (!initialized) {
         expect(result.length).toBe(3);
+        initialized = true;
       } else {
         expect(result.length).toBe(2);
       }
     });
-    tick(3000);
-    removed = true;
-    const article$ = service.removeArticle(3);
-    tick(3000);
+    service.removeArticle(3);
   }));
 
 });
